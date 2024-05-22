99054f917d5ccc8b49a288c21478e5c5f3afc4ec
Twigbang510 <truong.nguyen5102002@gmail.com>
2024-05-22 20:06:42 +0700

    fix: nearby search

--- src/module/googlemaps-place/googlemaps-place.service.ts
+++ src/module/googlemaps-place/googlemaps-place.service.ts
@@ -2,15 +2,16 @@ import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { Client } from '@googlemaps/google-maps-services-js';
 import { AppConfigService } from '../config/app-config.service';
 import { NearbySearchDto } from './dto/nearby-search.dto';
-import { PredictionServiceClient } from '@google-cloud/aiplatform';
+import { LanguageServiceClient } from '@google-cloud/language';
 
 @Injectable()
 export class GooglemapsPlaceService {
   private googleMapsClient: Client;
-  private aiPlatformClient: PredictionServiceClient;
+  private client: LanguageServiceClient;
 
   constructor(private configService: AppConfigService) {
     this.googleMapsClient = new Client({});
+    this.client = new LanguageServiceClient();
   }
 
   async getPlaceDetails(placeId: string) {
@@ -31,7 +32,6 @@ export class GooglemapsPlaceService {
           ],
         },
       });
-      console.log(response.data.result);
       return response.data.result;
     } catch (error) {
       console.log(error.message);
@@ -42,7 +42,7 @@ export class GooglemapsPlaceService {
     }
   }
 
-  async getNearbyPlaces(nearbySearchDto: NearbySearchDto) {
+  async getNearbyPlaces(nearbySearchDto: NearbySearchDto): Promise<any[]> {
     const { lat, lng, type, radius = 1500 } = nearbySearchDto;
     try {
       const response = await this.googleMapsClient.placesNearby({
@@ -53,9 +53,14 @@ export class GooglemapsPlaceService {
           key: this.configService.googleMapsKey,
         },
       });
-      const results = response.data.results.map((place) => ({
-        place_id: place.place_id,
-      }));
+
+      const results = await Promise.all(
+        response.data.results.map(async (place) => ({
+          ...place,
+          reviews: await this.getPlaceReviews(place.place_id),
+        })),
+      );
+
       return results;
     } catch (error) {
       throw new HttpException(
@@ -73,7 +78,13 @@ export class GooglemapsPlaceService {
           key: this.configService.googleMapsKey,
         },
       });
-      return response.data.result.reviews || [];
+
+      const reviews = response.data.result.reviews || [];
+
+      return {
+        place_id: placeId,
+        reviews: reviews,
+      };
     } catch (error) {
       console.error(error.response?.data || error.message);
       throw new HttpException(
@@ -83,46 +94,4 @@ export class GooglemapsPlaceService {
     }
   }
 
-  async analyzeSentiment(text: string): Promise<number> {
-    try {
-      const [response] = await this.aiPlatformClient.predict({
-        endpoint: `projects/${this.configService.googleProjectId}/locations/${this.configService.googleLocation}/endpoints/${this.configService.googleEndpointId}`,
-        instances: [{ content: text }],
-      });
-
-      const sentimentScore = response.predictions[0].sentiment || 0;
-      return sentimentScore;
-    } catch (error) {
-      console.error(error);
-      throw new HttpException(
-        'Error analyzing sentiment',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
-  }
-
-  async getAverageSentimentScore(reviews: string[]) {
-    const scores = await Promise.all(
-      reviews.map((review) => this.analyzeSentiment(review)),
-    );
-    const totalScore = scores.reduce((sum, score) => sum + score, 0);
-    return totalScore / scores.length;
-  }
-
-  async getNearbyPlacesWithSentiment(nearbySearchDto: NearbySearchDto) {
-    const places = await this.getNearbyPlaces(nearbySearchDto);
-    const placeDetails = await Promise.all(
-      places.map(async (place) => {
-        const reviews = await this.getPlaceReviews(place.place_id);
-        const reviewTexts = reviews.map((review) => review.text);
-        const averageSentimentScore =
-          await this.getAverageSentimentScore(reviewTexts);
-        return {
-          place_id: place.place_id,
-          averageSentimentScore,
-        };
-      }),
-    );
-    return placeDetails;
-  }
 }
